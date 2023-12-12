1fb9a8ee780c5616e8194bbca3087153c1a99fd8
Miguel Angel Valdez Navarrete <55571730+Mixcoatl120@users.noreply.github.com>
2023-12-12 12:44:48 -0600

    12/12/2023 #1

--- users/home/home_routes.py
+++ users/home/home_routes.py
@@ -9,53 +9,47 @@ home_u = Blueprint('home_u',__name__,template_folder = 'templates')
 @home_u.route('/home_u')
 @login_required
 def Home_u():
-    fechahoy = datetime.date.today()
+    #fechahoy = datetime.date.today()
+    fechahoy = '2023/03/30'
+
     # Materias
-    #   |     select    |   count  |          from         |                Where                    |          |
-    c = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy).scalar()
-    A = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '1').scalar()
-    CUS = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '2').scalar()
-    IA = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '3').scalar()
-    LBA = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '4').scalar()
-    RA = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '5').scalar()
-    T = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '6').scalar()
-    RP = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '7').scalar()
-    SC = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '8').scalar()
-    S = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '9').scalar()
-    SAS = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '10').scalar()
-    PD = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '11').scalar()
-    AA = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '12').scalar()
-    RME = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '13').scalar()
-    PRE = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '15').scalar()
-    ES = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '16').scalar()
-    M = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '17').scalar()
-    CDA = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '18').scalar() 
-    N = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '19').scalar()
-    TPPPMD = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '20').scalar()
-    YC = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '21').scalar()
-    YNC = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.materia == '22').scalar()
-    SM = A + CUS + IA + LBA + RA + T + RP + SC + S + SAS + PD + AA + RME + PRE + ES + M + CDA + N + TPPPMD + YC +YNC 
+    g_mat = (
+        db.session.query(
+            Materia.materia,
+            func.count(Seguimiento.materia).label('suma'))
+            .select_from(Seguimiento)
+            .join(Materia, Materia.id == Seguimiento.materia)
+            .filter(Seguimiento.fsolicitud == fechahoy)
+            .group_by(Materia.id)
+            .order_by(Materia.id)
+            .all())
+
+    labels_mat = [row.materia for row in g_mat] # se obtiene los nombres de las materias
+    data_mat = [row.suma for row in g_mat]# se obtienen los valores correspondientes de materias
+    c = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy).scalar() # se obtiene la suma total de tramites
+
+
     # Asuntos
-    JO = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '1').scalar()
-    DP = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '2').scalar()
-    CNDH = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '3').scalar()
-    OIC = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '4').scalar()
-    PGR = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '5').scalar()
-    JA = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '6').scalar()
-    JCA = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '7').scalar()
-    RR = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '8').scalar()
-    CON = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '9').scalar()
-    CUM = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '10').scalar()
-    I = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '11').scalar()
-    PT = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '12').scalar()
-    SO = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '13').scalar()
-    NOT = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '14').scalar()
-    AVI = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '15').scalar()
-    CC = db.session.query(func.count(Seguimiento.fsolicitud)).filter(Seguimiento.fsolicitud == fechahoy,Seguimiento.tipo_asunto == '16').scalar()
-    SA = JO + DP + CNDH + OIC + PGR + JA + JCA + RR + CON + CUM + I + PT + SO + NOT + AVI + CC
+    g_asu = (
+        db.session.query(
+            Asunto.tipo,
+            func.count(Seguimiento.tipo_asunto).label('suma'))
+            .select_from(Seguimiento)
+            .join(Asunto, Asunto.id == Seguimiento.tipo_asunto)
+            .filter(Seguimiento.fsolicitud == fechahoy)
+            .group_by(Asunto.id)
+            .order_by(Asunto.id)
+            .all())
+    
+    labels_asu = [row.tipo.strip() for row in g_asu] # se obtiene los nombres de las asunto
+    data_asu = [row.suma for row in g_asu]# se obtienen los valores correspondientes de asunto
+
+    d = db.session.query(func.count(Seguimiento.tipo_asunto)).filter(Seguimiento.fsolicitud == fechahoy, Seguimiento.tipo_asunto != 0).scalar() # se obtiene la suma total de tramites
+
 
     dg = Dir_Gen.query.filter_by(cve_unidad=2).all()# consulta a direccion general
 
+    # tabla
     resultados = (
         db.session.query(
             Materia.materia,
@@ -79,14 +73,13 @@ def Home_u():
     )
     total = 0
     res = []
-
+    
     for r in resultados:
         suma = 0
         for valor in r:
             if isinstance(valor, (int, float)):  # Verifica si el espacio contine un strig 
                 suma += valor
         total += suma # suma total de valores en sumas
-        #sumas.append(suma)# agrega la suma a la lista de sumas
 
         res.append({
             'Materia':r.materia,
@@ -102,18 +95,14 @@ def Home_u():
 
         })
 
-
     # Cierra la sesión después de usarla
     db.session.close()
 
-    return render_template('home_u.html',c=c, # C REPRESENTA EL TOTAL DE TRAMITES 
-                           # Estas variables son las que pertenecen a materia 
-                           A=A,CUS=CUS,IA=IA,LBA=LBA,RA=RA,T=T,RP=RP,SC=SC,S=S,SAS=SAS,PD=PD,AA=AA,RME=RME,PRE=PRE,
-                           ES=ES,M=M,CDA=CDA,N=N,TPPPMD=TPPPMD,YC=YC,YNC=YNC,
-                           # Estas variables pertenecen a Asuntos 
-                           JO=JO,DP=DP,CNDH=CNDH,OIC=OIC,PGR=PGR,JA=JA,JCA=JCA,RR=RR,CON=CON,CUM=CUM,I=I,PT=PT,SO=SO,NOT=NOT,AVI=AVI,CC=CC,
-                           # Estas son las sumas de cada categoria 
-                           SM=SM,SA=SA,
+    return render_template('home_u.html',
+                           c=c, # C REPRESENTA EL TOTAL DE TRAMITES
+                           labels_mat=labels_mat,data_mat=data_mat, # variables perteneciantes a materia
+                           d=d, # es el total de asuntos
+                           labels_asu=labels_asu,data_asu=data_asu,# variables pertenecientes Asuntos 
                            # Consultas
                            dg=dg,resultados=resultados,res=res
                            )
