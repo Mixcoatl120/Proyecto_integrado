408f114ec878e6bd55dfb8147923e969306fd438
Miguel Angel Valdez Navarrete <55571730+Mixcoatl120@users.noreply.github.com>
2024-01-08 13:00:28 -0600

    08/01/2024 #1

--- login/models/entities/Users.py
+++ login/models/entities/Users.py
@@ -1,7 +1,7 @@
 ﻿from werkzeug.security import check_password_hash
 from flask_login import UserMixin
 
-class User(UserMixin):
+class User(UserMixin): # contiene los requisitos de inicio de sesion
     def __init__(self, id, login, pswd, name="", active="")-> None:
         self.id = id
         self.login = login
@@ -10,8 +10,8 @@ class User(UserMixin):
         self.active = active
      
     def is_admin(self):
-        return self.login == 'admin'
+        return self.login == 'admin' # verifica si el perfil es el administrador
 
-    @classmethod
+    @classmethod # en caso de que las contraseñas esten encryptadas, desencripta y compara la contraseña dada
     def check_password(self, hashed_password, password):
         return check_password_hash(hashed_password, password)
